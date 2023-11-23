e72a7c002c300d27a07c538cf3ad8c7978102166
jhonrolyol <rolyordonezleon@gmail.com>
2023-11-23 11:09:34 -0500

    Change in r folder

---
+++ r/.Rproj.user/ACA7ED42/sources/session-e3eaae2c/64B3DF92-contents
@@ -0,0 +1,13 @@
+
+# Cleaning 
+  rm(list = ls())
+
+# Option 1
+  print("Hello world")
+# Option 2
+  hello_world <- "Hello world"
+  hello_world
+  
+  print(hello_world)
+
+
---
+++ r/vectors.R
@@ -0,0 +1,13 @@
+
+# Cleaning 
+  rm(list = ls())
+
+# Option 1
+  vec_1 <- c("Mother", "Father", "Son", "Sister")
+  vec_2 <- c(46, 54, 18, 11)
+
+# Option 2  
+  
+  
+  
+  
