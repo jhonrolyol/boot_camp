ba2be7ca576d5d3877c3ca97bc4f843badb32232
jhonrolyol <rolyordonezleon@gmail.com>
2023-11-24 16:46:37 -0500

    Change in python folder

--- python/VaR/VaR.py
+++ python/VaR/VaR.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Nov 24 12:38:59 2023
 
-@author: PC
+@author: Jhon Roly
 """
 
 
@@ -12,13 +12,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%% Import data
+#%% Single graph - 2D 
 
 x = [2 , 4, 6, 8]
 y = [1 , 5, 7, 10]
 
-
+plt.figure(figsize = (10,8))
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("y vs x")
 plt.plot(x,y)
+plt.savefig("figures/fig1.png")
 plt.show()
+
+#%% Data import 
+
 
 
