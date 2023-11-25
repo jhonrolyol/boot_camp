5afdb13fa4f4d54aa1f7b85b82ddcb7e531755fa
jhonrolyol <rolyordonezleon@gmail.com>
2023-11-24 19:53:10 -0500

    Change in r folder

--- r/VaR/VaR.R
+++ r/VaR/VaR.R
@@ -1,13 +1,67 @@
+# Documentation -----------------------------------------------------------
+  # Topic: VaR calculation (VaR: Value at Risk)
+  # Author: Jhon Roly
+  # Date: november 24, 2023
 
 
+# Shorcuts ----------------------------------------------------------------
+  # ctrl + s: save
+  # ctrl + shift + r: new section
+  # ...
+  
 
-# Cleaning 
+# Cleaning ----------------------------------------------------------------
   rm(list = ls())
 
+# Install packages --------------------------------------------------------
+  #install.packages("readxl")
+  #install.packages("tseries")
+  #install.packages("ggplot2")
+
+# Load library ------------------------------------------------------------
+  library(readxl)
+  library(tseries)
+  library(ggplot2)
+
+# Data import of excel ----------------------------------------------------
+  bcp <- read_excel("data/bcp.xlsx", sheet = "bbdd")
+  View(bcp)
+  
+  head(bcp)
+  
+  bcp$fecha <- as.Date(bcp$fecha)
+  
+
+# bcp series chart --------------------------------------------------------
+  graph1 <- ggplot(bcp, aes(x = fecha, y = bcp)) +
+    geom_line() +
+    labs(title = "bcp series chart",
+         x = "Date",
+         y = "Value") +
+    theme_minimal()
+  
+  graph1
+  
+  ggsave("figures/bcp_series_chart.png", plot = graph1)
   
   
-
-
-
-
-
+# r series chart ----------------------------------------------------------
+  graph2 <- ggplot(bcp, aes(x = fecha, y = r)) +
+    geom_line() +
+    labs(title = "r series chart",
+         x = "Date",
+         y = "Value") +
+    theme_minimal()
+  
+  ggsave("figures/r_series_chart.png", plot = graph2)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
