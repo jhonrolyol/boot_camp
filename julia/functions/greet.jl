edfabd8396b0d0872c19065918ee541550455783
jhonrolyol <rolyordonezleon@gmail.com>
2023-11-26 09:44:57 -0500

    My first commit in functions and loops folder

---
+++ julia/functions/greet.jl
@@ -0,0 +1,44 @@
+# Open the terminal: control + shift + enter
+# Open the command palette: control + shift + p
+# Run Julia: shift + enter
+
+# Install packages (type in the Julia environment)
+    # ] add Statistics
+    # ] add Distributions
+    # ] add LinearAlgebra
+    # ] add Plots
+    # ] add DataFrames
+    # ] add SymPy
+
+# Load useful packages
+using Statistics
+using Distributions
+using LinearAlgebra
+using Plots 
+using DataFrames
+using SymPy
+
+
+# option 1
+
+# Example: Define a greet function in Julia
+
+# Define the greet function
+function greet(name)
+    println("Hello, $name! Welcome to Julia.")
+end
+
+# Call the greet function with a specific name
+greet("Alice")
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ julia/loops/if.jl
@@ -0,0 +1,39 @@
+# Open the terminal: control + shift + enter
+# Open the command palette: control + shift + p
+# Run Julia: shift + enter
+
+# Install packages (type in the Julia environment)
+    # ] add Statistics
+    # ] add Distributions
+    # ] add LinearAlgebra
+    # ] add Plots
+    # ] add DataFrames
+    # ] add SymPy
+
+# Load useful packages
+using Statistics
+using Distributions
+using LinearAlgebra
+using Plots 
+using DataFrames
+using SymPy
+
+
+# option 1
+
+# Example: Loop with "if" statement in Julia
+
+# Define an array of numbers
+numbers = [1, 5, 8, 12, 4, 9, 7]
+
+# Loop through each number in the array
+for number in numbers
+    # Check if the number is even
+    if number % 2 == 0
+        println("$number is even")
+    else
+        println("$number is odd")
+    end
+end
+
+
